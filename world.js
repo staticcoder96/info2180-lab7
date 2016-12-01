07760f9e23c19eb1f5dc223e6190356a10d635eb
staticcoder96 <classiestephy67@yahoo.com>
2016-12-01 04:12:03 -0500

    files for lab 7

--- world.js
+++ world.js
@@ -1,5 +1,4 @@
-window.onload = function(){ 
-    
+window.onload = function(){ //To execute number 1 uncomment the code below Show Jamaica and the alert.
     document.getElementById("lookup").onclick = function() {looking();};
     
         function looking(){
@@ -10,23 +9,65 @@ window.onload = function(){
             
             if (xxhttp.readyState === 4 && xxhttp.status === 200)
             {
-                document.getElementById("result").innerHTML = xxhttp.responseText;
-                //alert(xxhttp.responseText);
+               //alert(xxhttp.responseText);
+               document.getElementById("result").innerHTML = xxhttp.responseText;
+    
             }
+           
        };
-       
-       //if ()
-        
-        //xxhttp.open('GET',"world.php?country=" +c, true);
-        
-         xxhttp.open('GET',"world.php?all=true", true);
-         xxhttp.send();
+        xxhttp.open("GET","world.php?country=" +c, true);
+        xxhttp.send();
+    }
+
+
+
+    
+    
+    document.getElementById("all").onclick = function() {Every();};
+   // document.getElementById("lookup").onclick = function() {Every();};
+    
+     function Every(){
+            //set up Ajax request
+            var dhttp = new XMLHttpRequest();
+            var c = document.getElementById("country").value;
+            var a = document.getElementById("all").checked;
+            var b = document.getElementById("lookup");
+            
+            dhttp.onreadystatechange = function() {
+                if (dhttp.readyState === 4 && dhttp.status === 200){ 
+                    a.addEventListener("click",function(){
+                        b.addEventListener("click", function(){
+                            document.getElementById("result").innerHTML = dhttp.responseText;
+                            
+                        });
+                        
+                         
+                    });
+                }
+            };
+            
+             dhttp.open("GET","world.php?+all=true", true);
+             dhttp.send();
              
-            
-        }
-            
-       
-   
+            if (c == ""){
+                document.getElementById("result").innerHTML = "Enter the Name of a Country Please!!!";
+             }
+             else{
+                  var xxhttp = new XMLHttpRequest(); //Ajax request
+                  var c = document.getElementById("country").value;
+                  
+                  xxhttp.onreadystatechange = function(){
+                      if (xxhttp.readyState === 4 && xxhttp.status === 200){
+                          alert(xxhttp.responseText);
+                          document.getElementById("result").innerHTML = xxhttp.responseText;
+                        }
+                    };
+                    xxhttp.open("GET","world.php?country=" +c, true);
+                    xxhttp.send();
+                }
+                
+     }        
+        
+}; //end of entire function
     
-    
-};
+        
--- world.php
+++ world.php
@@ -10,7 +10,7 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password); //conn
 
 $stmt = $conn->query("SELECT * FROM countries");
 $want = $conn->query("SELECT * FROM countries WHERE name LIKE '%$coun%' ");//from your connection get information (query)
-$every = $conn->query("SELECT * FROM countries");
+
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $show = $want->fetchAll(PDO::FETCH_ASSOC);//gets information
@@ -21,10 +21,12 @@ foreach ($results as $row) {
 }
 echo '</ul>';
 
+//Shows only jamaica
 /*echo '<ul>';
 foreach ($show as $row) {
   echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
 }
 echo '</ul>';*/
 
-//?>
+
+?>
